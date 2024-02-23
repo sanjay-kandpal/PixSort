c5870aba31afe4259229f38c2fd35ca9dc6e20fc
sanjay <72781686+sanjay-kandpal@users.noreply.github.com>
2024-02-24 00:13:59 +0530

    20% landing done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import Signup from './Signup';
-import Home from './Home';
-import LandingPage from './LandingPage';
-import HandleFileUpload from './HandleFileUpload';
-
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./component/Login";
 import Signup from "./component/Signup";
 import Home from "./component/Home";
 import HandleFileUpload from "./component/HandleFileUpload";
->>>>>>> ec5749685f49bbc7162c558b485509a869324de7
+import LandingPage from "./component/LandingPage";
+
 function App() {
   return (
     <BrowserRouter>
@@ -22,13 +13,9 @@ function App() {
         <Route path="/Login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-        <Route path="/landing" element={<LandingPage />} />
-        <Route path='/Upload' element={<HandleFileUpload />} />
-=======
         <Route path="/home" element={<div>hello</div>} />
         <Route path="/Upload" element={<HandleFileUpload />} />
->>>>>>> ec5749685f49bbc7162c558b485509a869324de7
+        <Route path="/landing" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
