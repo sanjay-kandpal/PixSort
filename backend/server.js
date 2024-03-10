1ebf291f1a33050d51b58492122a5b3161c4ccbf
Adhish Bahl <adhishbahl0@gmail.com>
2024-03-10 18:17:04 +0530

    files uploading to s3 and data saving to mysql

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,35 @@
 const cookieParser = require('cookie-parser');
 const express = require('express')
-const multer = require('multer');
 const mysql = require('mysql');
 const cors = require('cors');
-const fs = require('fs').promises
 const session = require('express-session');
 const path = require('path');
-const app = express();
-const crypto = require('crypto');
 const bodyParser = require('body-parser');
+const cookie = require('cookie');
+
+const app = express();
 
 app.use(cors({
   origin: ["http://localhost:3000"],
-  methods: ["GET","POST"],
+  methods: ["GET", "POST", "OPTIONS"],
   credentials: true
 }));
+
+var userid = 0;
+
+app.use(session({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  } //set the session cookie properties
+}))
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(session({
-    secret: "secret",
-    resave: false,
-    saveUnitialized: false,
-    cookie: {
-      secure: false,
-      maxAge: 1000 * 60 * 60 * 24 * 7
-    } //set the session cookie properties
-}))
 
 
 app.post('/signout', (req, res) => {
@@ -44,29 +46,11 @@ app.post('/signout', (req, res) => {
   });
 });
 
-
-
-// Function to generate a random 6-digit code
-const generateRandomCode = () => {
-  return crypto.randomBytes(3).toString('hex').toUpperCase();
-}
-
-// Function to create a directory with a random name
-const createRandomDirectory = () => {
-  const randomCode = generateRandomCode();
-  const directoryName = `./uploads/${randomCode}`;
-  return directoryName;
-};
-
-// Multer configuration
-const storage = multer.memoryStorage(); // Use memory storage for simplicity
-const upload = multer({ storage: storage });
-
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root123",
-    database: "signup"
+  host: "localhost",
+  user: "root",
+  password: "root123",
+  database: "signup"
 })
 
 db.connect((err) => {
@@ -79,86 +63,113 @@ db.connect((err) => {
 
 
 // Handle file upload
-app.post('/upload', upload.array('files'), async(req, res) => {
-  try {
-    // Access uploaded files in req.files array
-    const files = req.files;
+// app.post('/upload', upload.array('files'), async (req, res) => {
+//   try {
+//     // Access uploaded files in req.files array
+//     const files = req.files;
 
-    // Create a random directory
-    const randomDirectory = createRandomDirectory();
-    await fs.mkdir(randomDirectory);
+//     // Create a random directory
+//     const randomDirectory = createRandomDirectory();
+//     await fs.mkdir(randomDirectory);
 
-    // Process and save files to the random directory
-    const uploadedFiles = [];
-    for (const file of files) {
-      const filename = `${file.originalname}`;
-      const filePath = path.join(randomDirectory, filename);
+//     // Process and save files to the random directory
+//     const uploadedFiles = [];
+//     for (const file of files) {
+//       const filename = `${file.originalname}`;
+//       const filePath = path.join(randomDirectory, filename);
 
-      // Save file to disk
-      await fs.writeFile(filePath, file.buffer);
+//       // Save file to disk
+//       await fs.writeFile(filePath, file.buffer);
 
-      uploadedFiles.push({
-        originalname: file.originalname,
-        filename: filename,
-        filePath: filePath,
-      });
+//       uploadedFiles.push({
+//         originalname: file.originalname,
+//         filename: filename,
+//         filePath: filePath,
+//       });
+//     }
+//     const randomCode = path.basename(randomDirectory);
+
+//     // Send a response with information about the uploaded files and directory
+//     res.status(200).json({ message: 'Files uploaded successfully', randomCode: randomCode });
+//   } catch (error) {
+//     // Handle errors
+//     console.error('An error occurred during file upload:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
+app.post('/signup', (req, res) => {
+  const sql = "INSERT INTO signup(`name`,`email`,`password`) VALUES (?)";
+  const values = [
+    req.body.name,
+    req.body.email,
+    req.body.password
+  ]
+  console.log(values);
+  db.query(sql, [values], (err, data) => {
+    if (err) {
+      console.log(err.message);
+      return res.json({ error: err.message });
     }
-    const randomCode = path.basename(randomDirectory);
-    
-    // Send a response with information about the uploaded files and directory
-    res.status(200).json({ message: 'Files uploaded successfully', randomCode: randomCode});
-  } catch (error) {
-    // Handle errors
-    console.error('An error occurred during file upload:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }});
-
-app.post('/signup',(req,res)=>{
-    const sql = "INSERT INTO signup(`name`,`email`,`password`) VALUES (?)";
-    const values = [
-        req.body.name,
-        req.body.email,
-        req.body.password
-    ]
-    console.log(values);
-    db.query(sql,[values],(err,data) => {
-      if(err){
-        console.log(err.message);
-        return res.json({error: err.message});
-      }  
-      console.log(data);
-      return res.json(data);
-    })
+    console.log(data);
+    return res.json(data);
+  })
 
 })
 
-app.get('/',(req,res)=>{
-  if(req.session.username ) {
-   return res.json({valid: true,username: req.session.username,cookie: req.cookies});
-  }else{
-    return res.json({valid: false});
+app.get('/', (req, res) => {
+  if (req.session.username) {
+    return res.json({ valid: true, username: req.session.username, cookie: req.cookies });
+  } else {
+    return res.json({ valid: false });
   }
 
 })
 
-app.post('/login',(req,res)=>{
-    const sql = "SELECT * FROM signup WHERE `email` = ? AND `password` = ?";
-    db.query(sql,[req.body.email,req.body.password],(err,data) => {
-      if(err){
-        return res.json({error: err.message});
-      }  
-      if(data.length > 0){
-        req.session.username = data[0].name;
-        console.log(req.session.username);
-        return res.json({message: true});
-      }else{
-        return res.json({message: false}); 
-      }
-    })
+app.post('/login', (req, res) => {
+  const sql = "SELECT * FROM signup WHERE `email` = ? AND `password` = ?";
+  db.query(sql, [req.body.email, req.body.password], (err, data) => {
+    if (err) {
+      return res.json({ error: err.message });
+    }
+    if (data.length > 0) {
+      req.session.username = data[0].name;
+      req.session.userid = data[0].id;
+      userid = req.session.userid;
+      console.log(req.session.username);
+      console.log(req.session.userid);
+      return res.json({ message: true });
+    } else {
+      return res.json({ message: false });
+    }
   })
+})
+
+app.post('/upload', (req, res) => {
+  const { partyCode, title } = req.body;
+
+  const sql = 'INSERT INTO albums (partyCode, title, date, owner) VALUES (?, ?, NOW(), ?)';
+
+  if (userid != 0) {
+    console.log("inside")
+    db.query(sql, [partyCode, title, userid], (err, result) => {
+      console.log("inside inside")
+      if (err) {
+        console.error('MySQL query error:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+      } else {
+        console.log('Upload data inserted into MySQL');
+        res.status(200).json({ message: 'Upload successful', user: userid });
+      }
+    });
+  } else {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+
+});
 
 
 
-app.listen(8081,()=>{
-    console.log('listening');
+app.listen(8081, () => {
+  console.log('listening');
 })
--- frontend/src/component/FileUpload.jsx
+++ frontend/src/component/FileUpload.jsx
@@ -1,8 +1,12 @@
 import React, { useCallback, useState, useEffect } from "react";
 import { useDropzone } from "react-dropzone";
 import Sidebar from "./Sidebar";
-import { BiSearch } from "react-icons/bi";
+// import { BiSearch } from "react-icons/bi";
 import "../styles/fileUpload.css"
+// import axios from "axios";
+// import ReactS3 from "react-s3";
+import AWS from 'aws-sdk';
+
 
 const dropzoneStyle = {
 	flex: 1,
@@ -28,6 +32,11 @@ const dropzoneStyle = {
 const activeDropzoneStyle = {
 	borderColor: "#00adb5",
 };
+
+const BUCKET_NAME = process.env.REACT_APP_BUCKET_NAME
+const REGION = process.env.REACT_APP_REGION
+const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY
+const SECRET_KEY = process.env.REACT_APP_SECRET_KEY
 
 const FileUpload = () => {
 	const [files, setFiles] = useState([]);
@@ -58,11 +67,69 @@ const FileUpload = () => {
 		);
 	}, []);
 
-	const onUploadClick = () => {
-		// Add your upload logic here, for example, sending files to the server.
+	async function onUploadClick(e) {
+		e.preventDefault();
+
+
+		if (files.length === 0) {
+			alert("Please select a file to upload");
+			return;
+		}
+
+		if (files.length > 20) {
+			alert("You can only upload 20 files at a time")
+			return
+		}
+
+		if (!title) {
+			alert("Please give a title to the album")
+			return
+		}
+
 		console.log("Uploading files:", files);
-		// Clear the files after uploading if needed.
+
+		const s3 = new AWS.S3({
+			accessKeyId: ACCESS_KEY,
+			secretAccessKey: SECRET_KEY,
+			region: REGION,
+		});
+
+		const promises = files.map((file, index) => {
+			const params = {
+				Bucket: BUCKET_NAME,
+				Key: `${partyCode}/${file.name}`,
+				Body: file,
+				//   ACL: 'public-read', // Adjust the ACL based on your requirements
+			};
+
+			return s3.upload(params).promise();
+		});
+
+		try {
+		await Promise.all(promises);
+		console.log('Files uploaded successfully');
 		setFiles([]);
+		const uploadData = {
+			partyCode,
+			title,
+		};
+		const response = await fetch('http://localhost:8081/upload', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(uploadData),
+		});
+
+		if (response.ok) {
+			console.log('Upload data sent to server successfully');
+		} else {
+			console.error('Error sending upload data to server');
+		}
+		} catch (error) {
+		console.error('Error uploading files:', error.message);
+		alert('Error uploading files. Please try again.');
+		}
 	};
 
 	const copyButtonClicked = (e) => {
@@ -73,9 +140,10 @@ const FileUpload = () => {
 
 	const { getRootProps, getInputProps, isDragActive } = useDropzone({
 		onDrop,
-		accept: "image/*",
+		// accept: "image/*",
 		// maxSize: 1024 * 1024 * 5,
-		// maxFiles: 2000,
+		maxFiles: 20,
+		name: "images",
 	});
 
 	const fileList = files.map((file) => (
@@ -90,29 +158,32 @@ const FileUpload = () => {
 			<Sidebar />
 			<div className="d-flex flex-column">
 				<div className="partyCodeDiv">
-					<input type="text" placeholder="Title" className="titleInput" value={title} />
+					<input type="text" placeholder="Title" className="titleInput" value={title} onChange={(e) => setTitle(e.target.value)} />
 					<b>Party Code:</b> {partyCode} <svg xmlns="http://www.w3.org/2000/svg" className="copyIcon" style={{ width: "1.3rem", margin: "0px 1rem" }} onClick={copyButtonClicked} viewBox="0 0 448 512"><path d="M384 336H192c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16l140.1 0L400 115.9V320c0 8.8-7.2 16-16 16zM192 384H384c35.3 0 64-28.7 64-64V115.9c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1H192c-35.3 0-64 28.7-64 64V320c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H256c35.3 0 64-28.7 64-64V416H272v32c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192c0-8.8 7.2-16 16-16H96V128H64z" /></svg>
 				</div>
-				<div
-					style={
-						isDragActive
-							? { ...dropzoneStyle, ...activeDropzoneStyle }
-							: dropzoneStyle
-					}
-					{...getRootProps()}
-				>
-					<input {...getInputProps()} />
-					<p className="DropzoneText" >
-						Drag and drop your files here, or click to select files
-					</p>
-					<ul className="ImageContainer">{fileList}</ul>
-				</div>
-				<button className="uploadButton" onClick={onUploadClick}>
-					Upload
-				</button>
-				<div className="imageDescription">
+				<form method="post" onSubmit={onUploadClick}>
+					<div
+						style={
+							isDragActive
+								? { ...dropzoneStyle, ...activeDropzoneStyle }
+								: dropzoneStyle
+						}
+						{...getRootProps()}
+					>
+						<input type="file" name="images" {...getInputProps()} />
+						<p className="DropzoneText" >
+							Drag and drop your files here, or click to select files
+						</p>
+						<ul className="ImageContainer">{fileList}</ul>
+					</div>
+					{/* <button className="uploadButton" onClick={onUploadClick}> */}
+					<button type="submit" className="uploadButton" >
+						Upload
+					</button>
+					<div className="imageDescription">
 
-				</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	);
